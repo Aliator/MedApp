aeb1465111aca9357b083d48de3adb213764b342
Aliba <Aliba@ALIPC>
2026-01-30 17:20:15 +0000

    Initial Application Setup Created Commands, Queries and Validators Created Repository Inferface Updated Project names to include MedApp in namespace

---
+++ MedApp.Application/Patients/Commands/CreatePatient/CreatePatientCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace MedApp.Application.Patients.Commands.CreatePatient;
+
+public sealed record CreatePatientCommand(
+    string FirstName,
+    string LastName,
+    DateTime DateOfBirth,
+    string? Email
+) : IRequest<Guid>;
---
+++ MedApp.Application/Patients/Commands/DeletePatient/DeletePatientCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace MedApp.Application.Patients.Commands.DeletePatient;
+
+public sealed record DeletePatientCommand(Guid Id) : IRequest;
