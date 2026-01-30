36a521b4d46cb77616c805e5bdcccde3c0bf1acc
Aliba <Aliba@ALIPC>
2026-01-30 18:26:53 +0000

    Added initial infrastructure shell Added EF initial migration and DB connection

---
+++ MedApp.Infrastructure/Data/Repositories/PatientRepository.cs
@@ -0,0 +1,47 @@
+﻿using MedApp.Application.Patients.Repositories;
+using MedApp.Domain.Patients;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedApp.Infrastructure.Data.Repositories;
+
+public sealed class PatientRepository(MedAppDbContext context) : IPatientRepository
+{
+    public async Task AddAsync(Patient patient, CancellationToken cancellationToken)
+    {
+        context.Patients.Add(patient);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task UpdateAsync(Patient patient, CancellationToken cancellationToken)
+    {
+        context.Patients.Update(patient);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task DeleteAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var patient = await context.Patients.FindAsync([id], cancellationToken);
+
+        if (patient is null)
+        {
+            return;
+        }
+
+        context.Patients.Remove(patient);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<Patient?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return await context.Patients
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+    }
+
+    public async Task<IEnumerable<Patient>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return await context.Patients
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+    }
+}
