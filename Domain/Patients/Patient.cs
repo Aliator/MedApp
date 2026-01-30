be3d331d653a6008dd9e1f3d98495083d23e1a5e
Aliba <Aliba@ALIPC>
2026-01-30 16:58:37 +0000

    Added patient domain object

---
+++ Domain/Patients/Patient.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Patients;
+
+public sealed class Patient
+{
+    public Guid Id { get; init; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime DateOfBirth { get; init; }
+    public string? Email { get; set; }
+    public DateTime CreatedAt { get; init; }
+    public DateTime LastUpdated { get; set; }
+}
